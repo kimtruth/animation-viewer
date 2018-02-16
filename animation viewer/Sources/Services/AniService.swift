6b233c445a165a21ec6f2733ea9275c9fed9432d
j28150 <j28150@naver.com>
2018-02-16 22:41:13 +0900

    AniService.info() 작성

---
+++ animation viewer/Sources/Services/AniService.swift
@@ -0,0 +1,25 @@
+//
+//  AniService.swift
+//  animation viewer
+//
+//  Created by Truth on 2018. 2. 16..
+//  Copyright © 2018년 kimtruth. All rights reserved.
+//
+
+import Alamofire
+
+struct AniService: APIServiceType {
+  static func info(completion: @escaping (DataResponse<Any>) -> Void) {
+    let urlString = self.url("/json")
+    let headers = [
+      "Accpet": "application/json"
+    ]
+    
+    Alamofire
+      .request(urlString, headers: headers)
+      .validate(statusCode: 200..<400)
+      .responseJSON { response in
+        completion(response)
+    }
+  }
+}
