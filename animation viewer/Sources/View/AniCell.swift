190da2f76899d91e276f9ff6362b8dd969a78906
j28150 <j28150@naver.com>
2018-02-16 21:08:55 +0900

    AniCell 추가

---
+++ animation viewer/Sources/View/AniCell.swift
@@ -0,0 +1,21 @@
+//
+//  AniCell.swift
+//  animation viewer
+//
+//  Created by Truth on 2018. 2. 16..
+//  Copyright © 2018년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+class AniCell: UICollectionViewCell {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+}
