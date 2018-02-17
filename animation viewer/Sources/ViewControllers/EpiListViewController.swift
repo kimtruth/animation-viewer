9f4e940bceda269eb1f2dd1fa4755e24207aed65
j28150 <j28150@naver.com>
2018-02-17 12:58:34 +0900

    EpiListViewConroller 작성

---
+++ animation viewer/Sources/ViewControllers/EpiListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EpiListViewController.swift
+//  animation viewer
+//
+//  Created by Truth on 2018. 2. 17..
+//  Copyright © 2018년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+final class EpiListViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
