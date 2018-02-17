b14f5987bc649d1f1592de31e232d1b7669f4484
j28150 <j28150@naver.com>
2018-02-17 11:17:07 +0900

    SideMenuViewController 작성

---
+++ animation viewer/Sources/ViewControllers/SideMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuViewController.swift
+//  animation viewer
+//
+//  Created by Truth on 2018. 2. 17..
+//  Copyright © 2018년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+final class SideMenuViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
