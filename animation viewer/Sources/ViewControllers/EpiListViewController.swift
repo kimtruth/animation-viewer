f5e399dc8398574b3d84889827e0d08c610e9296
j28150 <j28150@naver.com>
2018-02-17 13:05:29 +0900

    임시로 cell을 리턴하게 함

--- animation viewer/Sources/ViewControllers/EpiListViewController.swift
+++ animation viewer/Sources/ViewControllers/EpiListViewController.swift
@@ -39,4 +39,13 @@ extension EpiListViewController: UITableViewDelegate {
 
 extension EpiListViewController: UITableViewDataSource {
   
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    
+    return cell
+  }
 }
